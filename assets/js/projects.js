5a553b4968be3385aab657e5307f4be93c4386db
Paulo Salvatore <paulo@salvatore.tech>
2020-08-24 18:43:26 -0300

    Projects elements creation and color picking from json data

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -14,3 +14,74 @@ Pode baixar só o áudio e só o vídeo
 - Quando só tem áudio:
 Download só do áudio
 */
+
+// Load JSON data
+
+let projects = [];
+let colors = [];
+let tags = [];
+
+const createColorsClasses = () => {
+    const style = document.createElement('style');
+    style.type = 'text/css';
+
+    colors.forEach((color, index) => {
+        style.innerHTML += `.tiles article.style_${index} > .image:before { background-color: #${color}; }\n`;
+    });
+
+    document.getElementsByTagName('head')[0].appendChild(style);
+};
+
+$.getJSON('data/colors.json', data => {
+    colors = data;
+
+    createColorsClasses();
+
+    dataReady();
+});
+
+$.getJSON('data/projects.json', data => {
+    projects = data;
+
+    dataReady();
+});
+
+$.getJSON('data/tags.json', data => {
+    tags = data;
+
+    dataReady();
+});
+
+const projectBaseElement = $('#project_base');
+
+const createProjectElement = (project, index) => {
+    const cloneElement = projectBaseElement.clone();
+    cloneElement.attr('id', '');
+
+    cloneElement.addClass('style_' + index)
+
+    // Change image
+
+    const imageUrl = 'https://img.youtube.com/vi/nK0L51DUf9I/hqdefault.jpg';
+    const imageElement = cloneElement.find('.image');
+
+    imageElement.find('img').attr('src', imageUrl);
+
+    // Add new element to projectBaseElement's parent
+
+    projectBaseElement.parent().append(cloneElement);
+};
+
+const createProjectElements = projects => {
+    projects.forEach(createProjectElement);
+};
+
+const dataReady = () => {
+    if (!projects.length
+        || !colors.length
+        || !tags.length) {
+        return;
+    }
+
+    createProjectElements(projects);
+};
