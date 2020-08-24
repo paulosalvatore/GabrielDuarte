7d30e58db21aefbe90c134ef176145c36ea0f451
Paulo Salvatore <paulo@salvatore.tech>
2020-08-24 18:16:59 -0300

    Removed comments from html and moved to projects.js

---
+++ assets/js/projects.js
@@ -0,0 +1,16 @@
+/*
+https://youtu.be/nK0L51DUf9I
+https://www.youtube.com/watch?v=nK0L51DUf9I&feature=youtu.be
+
+Extrair ID: nK0L51DUf9I
+
+Transformar em: https://www.youtube.com/embed/{video_id}
+*/
+
+/*
+// Funcionamento
+- Quando tem vídeo:
+Pode baixar só o áudio e só o vídeo
+- Quando só tem áudio:
+Download só do áudio
+*/
